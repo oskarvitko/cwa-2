ac93f35006215e6943524a4e86fa7d3c11c3594e
usr1-15 <usr1-15@study.MIK.by>
2021-11-30 12:14:51 +0300

    dkfgjldfkjgd;fjh

--- routes/login-routes.js
+++ routes/login-routes.js
@@ -2,10 +2,36 @@ const { Router } = require('express')
 const config = require('../config')
 const router = Router()
 
-router.get('/', (req, res) => res.status(200).send(process.env.SURNAME_EN))
+router.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+    res.status(200).send(process.env.SURNAME_EN)
+})
 
-router.get('/ru', (req, res) => res.status(200).sendFile(config.rootDir + '/pages/surname.html'))
+router.get('/ru', (req, res) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    res.setHeader('Accept-Language', 'ru-RU')
+    res.status(200).send(code)
+})
 
-router.get('/by', (req, res) => res.status(200).sendFile(config.rootDir + '/pages/surname.html'))
+router.get('/code', (req, res) => {
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+    res.setHeader('Accept-Language', 'by-BY')
+    res.status(200).send(code)
+})
 
 module.exports = router
+
+const code = `
+    <html>
+        <head>
+            <title>Login</title>
+            <style>
+                body {
+                    font-weight: 700;
+                    font-size: 18px;
+                }
+            </style>
+        </head>
+        <body>${process.env.SURNAME_RU}</body>
+    </html>
+`
--- routes/promise-routes.js
+++ routes/promise-routes.js
@@ -7,4 +7,15 @@ router.get('/', (req, res) => {
     res.status(200).send(PROMISE)
 })
 
+router.get('/:x', async (req, res) => {
+    const { x } = req.params
+    if (Number.isNaN(Number(x))) return res.status(403).send(`Bad request: "${x}" is not a number`)
+    const result = await task(x)
+    res.status(200).send(result)
+})
+
 module.exports = router
+
+function task(x){
+    return new Promise((resolve, reject) => (x < 13) ? resolve('yes') : resolve('no'))
+}
